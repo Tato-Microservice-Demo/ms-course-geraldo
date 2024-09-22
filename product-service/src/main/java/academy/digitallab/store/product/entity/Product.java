0b2d5cb5401bd943581768871d244e7ecc5416aa
Geraldo <geraldo62012@gmail.com>
2024-09-22 15:27:54 -0500

    config-service

---
+++ product-service/src/main/java/academy/digitallab/store/product/entity/Product.java
@@ -0,0 +1,41 @@
+package academy.digitallab.store.product.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table( name = "tbl_products")
+@Data
+@AllArgsConstructor @NoArgsConstructor @Builder
+public class Product {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private Long id;
+    @NotEmpty(message = "El campo no debe ser vacío")
+    private String name;
+    private String description;
+    @Positive(message = "El stock debe ser mayor que cero")
+    private Double stock;
+    private Double price;
+    private String status;
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+
+    @NotNull(message = "La categoría no puede ser vacía")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Category category;
+
+
+}
